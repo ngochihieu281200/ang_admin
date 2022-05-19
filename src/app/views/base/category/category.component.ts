5249e67ef261bddeecf647574a9a9ebd28989cda
Ngô Chí Hiếu <hieu0949667684@gmail.com>
2022-05-19 10:10:10 +0700

    table

--- src/app/views/base/category/category.component.ts
+++ src/app/views/base/category/category.component.ts
@@ -1,15 +1,110 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  template: `<ng2-smart-table
+    [settings]="settings"
+    [source]="source"
+    (create)="addStaff()"
+    (custom)="onCuston($event)"
+  ></ng2-smart-table>`,
+  styleUrls: ['./category.component.scss'],
 })
 export class CategoryComponent implements OnInit {
+  ngOnInit(): void {}
+  settings = {
+    mode: 'external',
+    actions: {
+      custom: [
+        {
+          name: 'editProduct',
+          title: '<i class="fa fa-edit icon-edit" title="Edit"></i>',
+        },
+        {
+          name: 'deleteProduct',
+          title: '<i class="fa fa-trash icon-delete" title="Delete"></i>',
+        },
+        {
+          name: 'detailProduct',
+          title: '<i class="fa fa-eye icon-detail" title="Detail"></i>',
+        },
+      ],
+      edit: false,
+      delete: false,
+      position: 'right',
+    },
+    columns: {
+      Id: {
+        title: 'Loại Đồng Hồ',
+        type: 'string',
+      },
+      Name: {
+        title: 'Dây Đeo',
+        type: 'string',
+      },
+      CategoryName: {
+        title: 'Mặt Đồng Hồ',
+        type: 'string',
+      },
+      BrandName: {
+        title: 'Dịch Vụ',
+        type: 'string',
+      },
+      // BrandName1: {
+      //   title: 'Email',
+      //   type: 'string',
+      // },
 
-  constructor() { }
-
-  ngOnInit(): void {
+      // Thumbnail: {
+      //   title: 'Hình Ảnh',
+      //   type: 'html',
+      //   valuePrepareFunction: (value) => {
+      //     return `<img src =  "${value}" width="100px">`;
+      //   },
+      // },
+      // hethong: {
+      //   title: 'Hệ Thống',
+      //   type: 'html',
+      // },
+    },
+  };
+  constructor(private router: Router) {}
+  onCuston(event) {
+    switch (event.action) {
+      case 'editProduct':
+        this.editProduct(event.data);
+        break;
+      case 'deleteProduct':
+        this.deleteProduct(event.data);
+        break;
+      // case 'detailProduct':
+      //   this.selectProduct(event.data);
+      //   break;
+    }
   }
-
+  source: LocalDataSource = new LocalDataSource();
+  addStaff() {
+    (<any>this.router).navigate([`/base/category/id`]);
+    console.log(this.addStaff);
+  }
+  editProduct(data) {
+    // (<any>this.router).navigate([`/base/product/${data.Id}`]);
+  }
+  deleteProduct(data) {
+    // this.idProduct = data.Id;
+    // this.modalReference = this.modalService.open(this.content);
+    // this.modalReference.result.then(
+    //   (result) => {
+    //     this.closeResult = `Closed with: ${result}`;
+    //   },
+    //   (reason) => {
+    //     this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    //   }
+    // );
+  }
+  selectProduct(data) {
+    // (<any>this.router).navigate([`/base/product-detail/${data.Id}`]);
+  }
 }
--- src/app/views/base/staff/staff.component.ts
+++ src/app/views/base/staff/staff.component.ts
@@ -1,15 +1,112 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LocalDataSource } from 'ng2-smart-table';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-staff',
-  templateUrl: './staff.component.html',
-  styleUrls: ['./staff.component.scss']
+
+  template: `<ng2-smart-table
+    [settings]="settings"
+    [source]="source"
+    (create)="addStaff()"
+    (custom)="onCuston($event)"
+  ></ng2-smart-table>`,
+  styleUrls: ['./staff.component.scss'],
 })
 export class StaffComponent implements OnInit {
+  [x: string]: any;
 
-  constructor() { }
+  ngOnInit(): void {}
+  settings = {
+    mode: 'external',
+    actions: {
+      custom: [
+        {
+          name: 'editProduct',
+          title: '<i class="fa fa-edit icon-edit" title="Edit"></i>',
+        },
+        {
+          name: 'deleteProduct',
+          title: '<i class="fa fa-trash icon-delete" title="Delete"></i>',
+        },
+        {
+          name: 'detailProduct',
+          title: '<i class="fa fa-eye icon-detail" title="Detail"></i>',
+        },
+      ],
+      edit: false,
+      delete: false,
+      position: 'right',
+    },
+    columns: {
+      Id: {
+        title: 'Mã Nhân Viên',
+        type: 'string',
+      },
+      Name: {
+        title: 'Tên Nhân Viên',
+        type: 'string',
+      },
+      CategoryName: {
+        title: 'Tài Khoản',
+        type: 'string',
+      },
+      BrandName: {
+        title: 'Chức Vụ',
+        type: 'string',
+      },
+      BrandName1: {
+        title: 'Email',
+        type: 'string',
+      },
 
-  ngOnInit(): void {
+      // Thumbnail: {
+      //   title: 'Hình Ảnh',
+      //   type: 'html',
+      //   valuePrepareFunction: (value) => {
+      //     return `<img src =  "${value}" width="100px">`;
+      //   },
+      // },
+      // hethong: {
+      //   title: 'Hệ Thống',
+      //   type: 'html',
+      // },
+    },
+  };
+  constructor(private router: Router) {}
+  onCuston(event) {
+    switch (event.action) {
+      case 'editProduct':
+        this.editProduct(event.data);
+        break;
+      case 'deleteProduct':
+        this.deleteProduct(event.data);
+        break;
+      // case 'detailProduct':
+      //   this.selectProduct(event.data);
+      //   break;
+    }
   }
-
+  source: LocalDataSource = new LocalDataSource();
+  addStaff() {
+    (<any>this.router).navigate([`/base/staff/id`]);
+    console.log(this.addStaff);
+  }
+  editProduct(data) {
+    // (<any>this.router).navigate([`/base/product/${data.Id}`]);
+  }
+  deleteProduct(data) {
+    // this.idProduct = data.Id;
+    // this.modalReference = this.modalService.open(this.content);
+    // this.modalReference.result.then(
+    //   (result) => {
+    //     this.closeResult = `Closed with: ${result}`;
+    //   },
+    //   (reason) => {
+    //     this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    //   }
+    // );
+  }
+  selectProduct(data) {
+    // (<any>this.router).navigate([`/base/product-detail/${data.Id}`]);
+  }
 }
