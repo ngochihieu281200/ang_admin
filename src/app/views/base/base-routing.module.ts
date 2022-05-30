29782bb8fd3819121072745470c856b1d0244de5
Nguyen Huu Tri <tri.nguyen@dgvdigital.com>
2022-05-30 22:22:16 +0700

    fix bug

--- src/app/views/base/base-routing.module.ts
+++ src/app/views/base/base-routing.module.ts
@@ -2,17 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { NavbarsComponent } from './navbars/navbars.component';
-import { ProductComponent } from './product/product.component';
 
 
-import { CustomerComponent } from './customer/customer.component';
-import { CategoryComponent } from './category/category.component';
-import { AddUpdateCategoryComponent } from './category/add-update-category/add-update-category.component';
-import { StaffComponent } from './staff/staff.component';
-import { AddUpdateStaffComponent } from './staff/add-update-staff/add-update-staff.component';
-
-import { ProductDetailComponent } from './product/product-detail/product-detail.component';
-import { AddUpdateProductComponent } from './product/add-update-product/add-update-product.component';
 
 const routes: Routes = [
   {
